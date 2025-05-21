a8b8795ca127dbe82c64626851099e5e0c5a78bd
Cauhê <cauhesanchesmatheus@gmail.com>
2025-05-21 15:27:33 -0300

    desafio 21 de maio adicionado

---
+++ desafio 21 de maio/dazzling_trug_gogo1.ino
@@ -0,0 +1,46 @@
+String nome[7];
+String email[7];
+int idade[7];
+void setup()
+{
+Serial.begin(9600);
+Serial.println("****** Bem vindo a Is The Party ******"); 
+Serial.println();  
+}
+
+void loop()
+{
+  for (int i = 0; i < 7; i++){
+  Serial.println("coloque o " + String(i + 1) + "o nome do convidado");
+  while ( ! Serial.available());
+  nome[i] = Serial.readString();
+  
+  Serial.println("coloque o email do " + nome[i]);
+  while ( ! Serial.available());
+  email[i] = Serial.readString();
+  
+  Serial.println("qual eh a idade do(a) " + nome[i]);
+  while ( ! Serial.available());
+  idade[i] = Serial.parseInt();
+ 
+  
+  }
+
+  Serial.println();
+  Serial.println("****** Lista dos convidados informados ******");
+  
+  for (int i = 0; i < 7; i++){
+  Serial.println("Nome: " + nome[i]);
+  Serial.println("Email: " + email[i]);
+  Serial.println("Idade: " + String(idade[i]));
+  Serial.println("-----");
+  }
+
+
+
+
+
+
+
+
+}
